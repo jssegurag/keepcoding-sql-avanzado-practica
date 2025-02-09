f9a6d62ec2b46875c3fb2095387c8b5e79f8de4e
Jhonatan Segura <js.segura@uniandes.edu.co>
2025-02-09 10:42:50 -0500

    se agrega el campo averia masiva

---
+++ 8.generar-campo-averia-masiva/generar-campo-averia-masiva.sql
@@ -0,0 +1,30 @@
+SELECT
+  c.ivr_id AS calls_ivr_id,
+  c.phone_number AS calls_phone_number,
+  c.ivr_result AS calls_ivr_result,
+  c.vdn_label AS calls_vdn_label,
+  c.start_date AS calls_start_date,
+  TO_CHAR(CAST(c.start_date AS timestamp), 'YYYYMMDD') AS calls_start_date_id,
+  c.end_date AS calls_end_date,
+  TO_CHAR(CAST(c.end_date AS timestamp), 'YYYYMMDD') AS calls_end_date_id,
+  c.total_duration AS calls_total_duration,
+  c.customer_segment AS calls_customer_segment,
+  c.ivr_language AS calls_ivr_language,
+  c.steps_module AS calls_steps_module,
+  c.module_aggregation AS calls_module_aggregation,será 1.
+  MAX(CASE WHEN m.module_name = 'AVERIA_MASIVA' THEN 1 ELSE 0 END) AS masiva_lg
+FROM public.ivr_calls c
+LEFT JOIN public."ivr_modules.csv" m
+  ON c.ivr_id::float = m.ivr_id
+GROUP BY 
+  c.ivr_id,
+  c.phone_number,
+  c.ivr_result,
+  c.vdn_label,
+  c.start_date,
+  c.end_date,
+  c.total_duration,
+  c.customer_segment,
+  c.ivr_language,
+  c.steps_module,
+  c.module_aggregation;
